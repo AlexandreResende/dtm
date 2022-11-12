2dcf6f6528664804f12dd70ee3048c3701559d18
Alexandre Resende <alexandre.amres@gmail.com>
2022-11-12 07:43:46 -0300

    fix: Adjust get contract to work properly

--- src/app.js
+++ src/app.js
@@ -1,24 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
+const { StatusCodes: { NOT_FOUND } } = require('http-status-codes');
+
+const { sequelize } = require('./model');
+const { ProfileType } = require('./enums/profileType');
+const { getProfile } = require('./middleware/getProfile');
+
 const app = express();
+
 app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
-const { StatusCodes: { NOT_FOUND } } = require('http-status-codes');
+const { ContractRepository } = require('./repositories/contractRepository');
 
+app.get('/contracts/:id', getProfile ,async (req, res) =>{
+    const {id} = req.params;
+    const { id: profileId, type: profileType } = req.profile;
+    let contract;
 
-/**
- * FIX ME!
- * @returns contract by id
- */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(NOT_FOUND).end()
-    res.json(contract)
-})
+    if (profileType === ProfileType.CLIENT) {
+        contract = await (new ContractRepository().findByIdAndClientId(id, profileId));
+    } else {
+        contract = await (new ContractRepository().findByIdAndContractorId(id, profileId));
+    }
+
+    if(!contract) {
+        return res.status(NOT_FOUND).end();
+    }
+
+    res.json(contract);
+});
+
 module.exports = app;
