d937f73fb7b0a836dc4fed2009adc0a50e1e7ab6
Alexandre Resende <alexandre.amres@gmail.com>
2022-11-12 10:17:28 -0300

    refactor: Update verifyProfile middleware

--- src/middleware/verifyProfile.js
+++ src/middleware/verifyProfile.js
@@ -6,7 +6,7 @@ const verifyProfile = async (req, res, next) => {
     const profile = req.profile;
 
     if (profile.type !== ProfileType.CLIENT) {
-      return res.status(FORBIDDEN).json({ message: 'Only clients can pay for jobs' });
+      return res.status(FORBIDDEN).end();
     }
 
     req.profile = profile
